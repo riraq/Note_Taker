c83c3b769243d57eab409b445670f67f47408240
riraq <riraq@mail.usf.edu>
2021-03-19 15:36:27 -0500

    added uuid; added post function for db when save icon clicked

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const path = require('path');
-const db = require('./db/db.json')
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
+const db = require('./db/db')
+const { v4: uuidv4 } = require('uuid');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
-    res.json(db)
+    res.json(db);
+})
+
+app.post('/api/notes', (req, res) =>{
+    req.body.id = uuidv4();
+    db.push(req.body);
+    console.log('this is the current data in the db: ', db);
+    res.end;
 })
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('public'));
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
