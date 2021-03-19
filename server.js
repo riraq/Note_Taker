b76fb3a14a8aa344819802612ca11eaacfad13c2
riraq <riraq@mail.usf.edu>
2021-03-19 13:22:37 -0500

    added a get for api/notes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const db = require('./db/db.json')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,9 +9,14 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
+app.get('/api/notes', (req, res) => {
+    res.json(db)
+})
+
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
